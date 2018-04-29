97f9be25119d7d564011004c28697fcd4c5bfa82
jkthomas <tom.januszek@gmail.com>
2018-04-29 20:04:41 +0200

    "[ViewModel] Changed viewmodel logic and directiory list types to observable collections"

---
+++ Helpers/SystemDirectories/WindowsDirectoryAcquireService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.SystemDirectories
+{
+    public class WindowsDirectoryAcquireService
+    {
+        public static List<string> GetComputerDrives()
+        {
+            var drives = Directory.GetLogicalDrives().ToList();
+
+            return drives;
+        }
+
+        public static List<string> GetDirectoryFolders(string path)
+        {
+            var dirs = Directory.EnumerateDirectories(path).ToList();
+            if (dirs.Equals(null))
+            {
+                throw new Exception("Empty directory");
+            }
+
+            return dirs;
+        }
+
+        public static List<string> GetDirectoryFiles(string path)
+        {
+            var files = Directory.EnumerateFiles(path).ToList();
+            if (files.Equals(null))
+            {
+                throw new Exception("Empty directory");
+            }
+            return files;
+        }
+
+        public static List<string> GetDiretoryTree(string path)
+        {
+            var tree = Directory.EnumerateFileSystemEntries(path).ToList();
+            if (tree.Equals(null))
+            {
+                throw new Exception("Empty directory");
+            }
+            return tree;
+        }
+
+        public static List<string> GetDirectoryParent(string path)
+        {
+            string parent = Directory.GetParent(path).ToString();
+            if (parent.Count() == 4)
+            {
+                return Directory.GetLogicalDrives().ToList();
+            }
+
+            return Directory.EnumerateFileSystemEntries(parent).ToList();
+        }
+    }
+}
---
+++ ViewModel/DirectoriesViewModel/DirectoryListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.SystemDirectories;
+
+namespace ViewModel.DirectoriesViewModel
+{
+    public class DirectoryListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public DirectoryListViewModel()
+        {
+            this.CurrentPath = null;
+            this.CurrentTree = new ObservableCollection<string>(WindowsDirectoryAcquireService.GetComputerDrives());
+        }
+
+        #region Current path and tree fields
+        private string _currentPath;
+        private ObservableCollection<string> _currentTree;
+        public string CurrentPath
+        {
+            get { return this._currentPath; }
+            set
+            {
+                if (this._currentPath == value)
+                    return;
+
+                this._currentPath = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CurrentPath)));
+            }
+        }
+
+        public ObservableCollection<string> CurrentTree
+        {
+            get { return this._currentTree; }
+            set
+            {
+                if (this._currentTree == value)
+                    return;
+
+                this._currentTree = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CurrentTree)));
+            }
+        }
+        #endregion
+
+
+    }
+}
